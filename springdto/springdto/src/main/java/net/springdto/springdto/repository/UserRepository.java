f17de2290ad68f96ed06d3f1c47f788f9ed6b3a7
Aiman Alias <aiman30801@gmail.com>
2023-09-04 10:39:52 +0800

    updated

--- springdto/springdto/src/main/java/net/springdto/springdto/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package net.springdto.springdto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.springdto.springdto.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-    
-}
